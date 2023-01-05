b5a1e55887400f907fb3853fdd2e5abb33e88cd8
Nick Bifle <nikitalm8@gmail.com>
2023-01-05 19:58:25 +0300

    Updated config.py

--- BlossomBot/config.py
+++ BlossomBot/config.py
@@ -1,6 +1,6 @@
-BOT_TOKEN = 'ODk0NTc3MzY5MTM2ODk4MDc4.YVsCGQ.Ed_UdLR39kxVuZU_mQqJoK4-nxE'
+BOT_TOKEN = 'YOUR-TOKEN-HERE'
 PREFIX = '!'
 
-DATABASE_URL = 'sqlite+aiosqlite:///./app/database/database.db'
+DATABASE_URL = ''  # DEFAULT SQLITE3 PATH: sqlite+aiosqlite:///./app/database/database.db
 
 data = {}
